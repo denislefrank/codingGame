ea3ac7707c47fea77a8251c3810bb1a3047dfba8
Denis Lefrank <denislefrank@gmail.com>
2025-02-15 13:22:21 +0100

    feat: added sum - added SumAction and abstract layer to provide future features - added actionType for same reason - refactored namings - changed slf4j implementation to work correctly - added test

--- src/main/java/ch/queo/cli/util/CommandLineParser.java
+++ src/main/java/ch/queo/cli/util/CommandLineParser.java
@@ -1,7 +1,7 @@
 package ch.queo.cli.util;
 
 import ch.queo.cli.dto.ParsedArgument;
-import ch.queo.cli.option.ArgumentOption;
+import ch.queo.cli.type.ArgumentType;
 import lombok.SneakyThrows;
 import lombok.experimental.UtilityClass;
 import lombok.val;
@@ -25,21 +25,21 @@ public class CommandLineParser {
     public ParsedArgument parseArguments(String[] args) {
 
         val options = new Options();
-        options.addOption(ArgumentOption.INPUT.getShortOption(), ArgumentOption.INPUT.getLongOption(), true, ArgumentOption.INPUT.getDescription());
-        options.addOption(ArgumentOption.OUTPUT.getShortOption(), ArgumentOption.OUTPUT.getLongOption(), true, ArgumentOption.OUTPUT.getDescription());
-        options.addOption(ArgumentOption.ACTION.getShortOption(), ArgumentOption.ACTION.getLongOption(), true, ArgumentOption.ACTION.getDescription());
+        options.addOption(ArgumentType.INPUT.getShortOption(), ArgumentType.INPUT.getLongOption(), true, ArgumentType.INPUT.getDescription());
+        options.addOption(ArgumentType.OUTPUT.getShortOption(), ArgumentType.OUTPUT.getLongOption(), true, ArgumentType.OUTPUT.getDescription());
+        options.addOption(ArgumentType.ACTION.getShortOption(), ArgumentType.ACTION.getLongOption(), true, ArgumentType.ACTION.getDescription());
 
         val parser = new DefaultParser().parse(options, args);
 
-        if (!parser.hasOption(ArgumentOption.INPUT.getShortOption()) || !parser.hasOption(ArgumentOption.ACTION.getShortOption())) {
+        if (!parser.hasOption(ArgumentType.INPUT.getShortOption()) || !parser.hasOption(ArgumentType.ACTION.getShortOption())) {
             throw new IllegalArgumentException("Missing Arguments: -i and -a are mandatory.");
         }
 
         return ParsedArgument
                 .builder()
-                .input(parser.getOptionValue(ArgumentOption.INPUT.getShortOption()))
-                .output(parser.getOptionValue(ArgumentOption.OUTPUT.getShortOption()))
-                .action(parser.getOptionValue(ArgumentOption.ACTION.getShortOption()))
+                .input(parser.getOptionValue(ArgumentType.INPUT.getShortOption()))
+                .output(parser.getOptionValue(ArgumentType.OUTPUT.getShortOption()))
+                .action(parser.getOptionValue(ArgumentType.ACTION.getShortOption()))
                 .build();
 
     }
--- src/test/java/ch/queo/cli/util/InputParserTest.java
+++ src/test/java/ch/queo/cli/util/InputParserTest.java
@@ -1,6 +1,5 @@
-package ch.queo.util;
+package ch.queo.cli.util;
 
-import ch.queo.cli.util.InputParser;
 import lombok.val;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
